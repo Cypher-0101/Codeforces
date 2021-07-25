d84186f689ac8c6abc8b44ab4eb72e4af979ec7a
KAUSTOV DUWARAH <87143297+Cypher-0101@users.noreply.github.com>
2021-07-25 22:56:09 +0530

    Codeforces Global Round 15

---
+++ 1555.B Running for Gold.cpp
@@ -0,0 +1,135 @@
+//The Grind continues//
+//Target is 1900++ By October//
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long int lli;
+typedef pair<int,int> ii;
+typedef tuple<int,int,int> iii;
+typedef unsigned long long int ull ;
+
+#define ll              long long
+#define vas()           vector<bool>is_prime(N,true);
+#define f1(n,m)         for(int i=0;i<n;i++)
+#define form()          cout<<done<<endl;
+#define ull             unsigned long long
+#define pb              push_back
+#define makefaster()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+ 
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define all(v) (v).begin(), (v).end()
+#define pll pair<lli,lli>
+#define prec(n) fixed<<setprecision(n)
+#define fast( ) (ios_base::sync_with_stdio(false);cin.tie(NULL), cout.tie(NULL);)
+#define vc vector<char>
+#define f7(i,a,n) for(ll i=a;i<n;i++)
+#define ve vector
+#define ss second
+#define ff first
+#define pf pop_front
+#define pob pop_back
+#define psf push_front
+#define o(m,n)         if(v[i][j]<v[y][j]) g++;
+#define p(k,l)         if(v[y][j]<v[i][j]) g++;
+#define w()            if(g>=3) y=i;
+#define as()           vector<int>temp;
+ 
+const int MOD=1e9+7;
+const char nl = '\n';
+const double pi = 2*acos(0.0);
+const double eps = 1e-9;
+ 
+#define M 1000000007
+
+vector<int>primes;
+int N=1e6+1;
+
+vas()
+
+
+void done()
+
+{
+    int k;
+    cin>>k;
+    
+    vector<vector<int>>v(k);
+    as()
+    f7(i,0,k)
+    {
+        f7(j,0,5)
+        
+        {
+            int x;
+            cin>>x;
+            v[i].push_back(x);
+        }
+        
+    }
+
+    int y=0;
+
+    f7(i,1,k){
+        int g=0;
+        f7(j,0,5){
+            o(m,n)
+        }
+
+        w()
+    }
+
+        bool r=true;
+            f7(i,0,k)
+            {
+            int g=0;
+            if(i!=y)
+            {
+            f7(j,0,5)
+            {
+                p(k,l)
+            }
+            if(g<3) 
+            r=false;
+        }
+}
+
+
+
+    if(k==1)
+    
+    {
+        cout<<1<<endl;
+    }
+    
+    else
+    
+    {
+        if(r)
+    
+        {
+            cout<<y+1<<endl;
+        }
+    
+        else
+    
+        {
+            cout<<-1<<endl;
+        }
+    }
+}
+
+int main()
+{
+    makefaster()
+    int t;
+    cin>>t;
+    while(t--)
+    {
+     done();
+    }
+    return 0;
+}
