4a9174fbd486406deb0ddc78b5e6fb3dab96609b
KAUSTOV DUWARAH <87143297+Cypher-0101@users.noreply.github.com>
2021-08-01 22:32:51 +0530

    Codeforces Round #736 (Div. 2)

---
+++ A. Gregor and Cryptography.cpp
@@ -0,0 +1,55 @@
+//The Grind continues//
+//Target is 1900++ By October//
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long int lli;
+typedef pair<int,int> ii;
+typedef tuple<int,int,int> iii;
+typedef unsigned long long int ull ;
+
+#define ll              long long
+#define vas()           vector<bool>is_prime(N,true);
+#define f()            for(int i=0;i<z;i++)
+#define form()          cout<<done<<endl;
+#define ull             unsigned long long
+#define pb              push_back
+#define makefaster()    (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+ 
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define all(v) (v).begin(), (v).end()
+#define pll pair<lli,lli>
+#define prec(n) fixed<<setprecision(n)
+#define vc vector<char>
+#define ve vector
+#define ss second
+#define ff first
+#define pf pop_front
+#define pob pop_back
+#define psf push_front
+
+const int MOD=1e9+7;
+const char nl = '\n';
+const double pi = 2*acos(0.0);
+const double eps = 1e-9;
+ 
+#define M 1000000007
+
+int main()
+{
+    ll t;
+    ll n;
+    cin>>t;
+    while(t--)
+    
+    {
+        cin>>n;
+        cout << 2 << " " << n-1 << "\n";
+    }
+    
+    return 0;
+}
