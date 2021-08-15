5ba22b3370eed92d9f4d764302cd4aa643534d8b
KAUSTOV DUWARAH <87143297+Cypher-0101@users.noreply.github.com>
2021-08-15 22:14:44 +0530

    Codeforces Round #738 (Div. 2)

---
+++ Mocha and Math.cpp
@@ -0,0 +1,99 @@
+//The Grind continues//
+//Target is 1900++ By October//
+ 
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <list>
+#include <time.h>
+#include <math.h>
+#include <random>
+#include <deque>
+#include <queue>
+#include <cassert>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <chrono>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+ 
+typedef long long int lli;
+typedef pair<int,int> ii;
+typedef tuple<int,int,int> iii;
+typedef unsigned long long int ull ;
+ 
+#define ll long long
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<pii
+#define vas()           vector<bool>is_prime(N,true);
+#define f()             for(int i=0;i<n;i++)
+#define form()          cout<<done<<endl;
+#define ull             unsigned long long
+#define pb              push_back
+#define makefaster()    (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+ 
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define all(v) (v).begin(), (v).end()
+#define pll pair<lli,lli>
+#define prec(n) fixed<<setprecision(n)
+#define vc vector<char>
+#define ve vector
+#define se second
+#define fi first
+#define pf pop_front
+#define pob pop_back
+#define psf push_front
+
+const int MOD=1e9+7;
+const char nl = '\n';
+const double pi = 2*acos(0.0);
+const double eps = 1e-9;
+ 
+#define M 1000000007
+
+const int mod = 1e9 + 7;
+
+int64_t t, n, m, k, a[1000005];
+string s;
+
+void done()
+{
+    int64_t ans = 0;
+    for(int j = 0; j <= 30; j++)
+    {
+        bool b = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            if(!(a[i] & (1 << j))) b = 1;
+        }
+        
+        if(!b) ans += (1 << j);
+    }
+    
+    cout << ans << "\n";
+}
+
+int main()
+{
+    makefaster()
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(int i = 1; i <= n; i++) cin >> a[i];
+        done();
+    }
+}
